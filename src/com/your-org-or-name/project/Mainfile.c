f5426496bee1cabe527ecc94733836306e3abc2a
Micah (Impolse) <micah@github.codedsoup.com>
2016-07-11 12:28:51 -0700

    Updated the Mainfile (gave it info)

--- src/com/your-org-or-name/project/Mainfile.c
+++ src/com/your-org-or-name/project/Mainfile.c
@@ -1,1 +1,8 @@
-// This is just an example for you.
+/*
+'
+' This is a example file, if you import/download/fork this project
+' you can rename the directory, file, and file extension.
+' Thank you for using Codedsoup Module.
+' - codedsoup.
+'
+*/
