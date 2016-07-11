b565d51d9aa30616d240a50fb5f76cc40255c733
Micah (Impolse) <micah@github.codedsoup.com>
2016-07-11 12:05:32 -0700

    Created the Mainfile of Codedsoup Module

---
+++ src/com/codedsoup/module/Module.py
@@ -0,0 +1,16 @@
+'''
+' Copyright (C) 2016 Codedsoup
+' Licensed under the MIT Licensed
+' https://opensource.org/licenses/MIT
+'''
+class main:
+  def info():
+    name = "project"
+    version = "v0.0"
+    description = "your project description"
+    developers = ["your", "authors"]
+
+class project:
+  def project():
+    print("Welcome to project.")
+    print("project is...");
