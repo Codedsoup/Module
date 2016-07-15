5f8915d79d1c93e56d074c53e1fdde40a1265792
Micah <micah@github.codedsoup.com>
2016-07-15 12:29:12 -0700

    Removed the Java file.

--- src/com/your-org-or-name/project/Mainfile.java
+++
@@ -1,8 +0,0 @@
-/*
-'
-' This is a example file, if you import/download/fork this project
-' you can rename the directory, file, and file extension.
-' Thank you for using Codedsoup Module.
-' - codedsoup.
-'
-*/
